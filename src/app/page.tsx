679bb31d7af5836e4c3675e8feeb367aabb989d3
Alfredo Leonelli Mendoza <alfredoleonellim@gmail.com>
2025-05-28 23:38:18 -0400

    update: change main page to under construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,13 @@ import AboutMe from "./components/aboutMe";
 import WorkExperience from "./components/workExperience";
 import Divider from "./components/divider";
 import Projects from "./components/projects";
+import PageUnderConstruction from "./pages/pageUnderConstruction";
 
 export default function Home() {
   return (
     <div>
       <link rel="icon" href="/images/alfredo.png" />
-      <NavBar></NavBar>
-      <Divider text="Sobre mi"></Divider>
-      <AboutMe></AboutMe>
-      <Divider text="Experiencia laboral"></Divider>
-      <WorkExperience></WorkExperience>
-      <Divider text="Proyectos"></Divider>
-      <Projects></Projects>
-      <Footer></Footer>
+      <PageUnderConstruction />
     </div>
   );
 }
